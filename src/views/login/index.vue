f323f81f09dbb871ec7bebad32ebab00a86a16ac
laukey2677 <laukey@126.com>
2020-03-04 10:38:08 +0800

    增加vue.config.js 文件配置， 修改页面路由，删除模板中不需要的vue组件模块

---
+++ src/views/login/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login">this is login page</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.login {
+  background-color: #344a5f;
+}
+</style>
