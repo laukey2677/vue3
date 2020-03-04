c256ca788df6d05ac019e0eae3b44944fc2deae2
laukey2677 <laukey@126.com>
2020-03-04 14:49:48 +0800

    vue3.0变更语法初探

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import store from "./store";
 import '@/assets/scss/common.scss';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import VueCompositionApi from '@vue/composition-api';
 
+Vue.use(VueCompositionApi);
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
